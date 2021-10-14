6e6235f656bce6f9a3676286cc4f84918fc6ad31
MontseGlezV <montserrat.gonzalez@thincode.com>
2021-10-14 09:57:50 -0500

    ejercicios1

--- ruby/exercises/promedio_array.rb
+++ ruby/exercises/promedio_array.rb
@@ -11,3 +11,13 @@
 #     Input: [1, 5, 87, 45, 8, 8] Resultado: 25
 #     Input: [2,5,13,20,16,16,10] Resultado: 11
 #     Input: [1,2,15,15,17,11,12,17,17,14,13,15,6,11,8,7] Resultado: 11
+
+arreglo = [10, 10, 9.3, 7.8, 8.5, 9.6, 8.7, 8.1]
+print "promedio: ",  arreglo.sum(0.0) / arreglo.size
+puts
+arreglo = [1, 5, 87, 45, 8, 8]
+print "Average: ", arreglo.sum(0) / arreglo.size
+p '        .'
+arreglo = [2,5,13,20,16,16,10]
+print "se pudo!!  = ", arreglo.sum(0.0) / arreglo.size
+p '        .'
