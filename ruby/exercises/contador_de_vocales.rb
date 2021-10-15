d48759dcc1abe56efaf1d7cba4d4f67d809fac3f
ernesto-martinez <ernesto.martinez@thincode.com>
2021-10-14 20:14:16 -0500

    Add average

--- ruby/exercises/contador_de_vocales.rb
+++ ruby/exercises/contador_de_vocales.rb
@@ -3,3 +3,14 @@
   # 'Ruby On Rails Course' => { a: 1, e: 1, i: 1, o: 2, u: 2 }
   # 'Gerardo' => { a: 1, e: 1, i: 0, o: 1, u: 0 }
   # 'BCDF' => { a: 0, e: 0, i: 0, o: 0, u: 0 }
+
+def cnt_vocal(str)
+  vowell = {a:0, e:0, i: 0, o: 0, u: 0}
+  str.chars.each do |chr|
+    next unless vowell.keys.include?(chr.to_sym)
+    vowell[chr.to_sym]+= 1
+  end
+  vowell
+end
+
+p cnt_vocal("hola")
