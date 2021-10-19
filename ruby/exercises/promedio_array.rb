8428a252f006df8b63b00cb2849878a7bdd35979
Hector <hectormartinez1471@gmail.com>
2021-10-18 22:55:03 -0500

    hmm-promedio

--- ruby/exercises/promedio_array.rb
+++ ruby/exercises/promedio_array.rb
@@ -11,3 +11,31 @@
 #     Input: [1, 5, 87, 45, 8, 8] Resultado: 25
 #     Input: [2,5,13,20,16,16,10] Resultado: 11
 #     Input: [1,2,15,15,17,11,12,17,17,14,13,15,6,11,8,7] Resultado: 11
+def promedio_e (calificaciones)
+    #con este si me da los resultados 2, 25, 11 y 11 --> porque la division la deja en entero
+    (calificaciones.sum / calificaciones.count).round
+
+end
+
+def promedio_f(calificaciones)
+    #si uso este me regresa 2, 26, 12 y 11 --> porque redondea el flotante
+    (calificaciones.sum.to_f / calificaciones.count).round
+
+end
+
+calificaciones  =  [2, 2, 2, 2] 
+calificaciones1 =  [1, 5, 87, 45, 8, 8]
+calificaciones2 =  [2,5,13,20,16,16,10]
+calificaciones3 =  [1,2,15,15,17,11,12,17,17,14,13,15,6,11,8,7]
+
+p 'Resultado entero'
+p promedio_e(calificaciones)
+p promedio_e(calificaciones1)
+p promedio_e(calificaciones2)
+p promedio_e(calificaciones3)
+
+p 'Resultado flotante'
+p promedio_f(calificaciones)
+p promedio_f(calificaciones1)
+p promedio_f(calificaciones2)
+p promedio_f(calificaciones3)
