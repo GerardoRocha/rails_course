6ad5df890a6fa12844cbea3e5bf1b580fc7ed129
Hector <hectormartinez1471@gmail.com>
2021-10-18 22:39:34 -0500

    hmm-contador-vocales

--- ruby/exercises/contador_de_vocales.rb
+++ ruby/exercises/contador_de_vocales.rb
@@ -3,3 +3,18 @@
   # 'Ruby On Rails Course' => { a: 1, e: 1, i: 1, o: 2, u: 2 }
   # 'Gerardo' => { a: 1, e: 1, i: 0, o: 1, u: 0 }
   # 'BCDF' => { a: 0, e: 0, i: 0, o: 0, u: 0 }
+  def cuentavocales!(stg)
+    vocales = {a: 0, e: 0, i: 0, o: 0, u: 0}
+    stg.chars.each do |chr|   
+      chr = chr.downcase
+      next unless vocales.keys.include?(chr.to_sym)
+              vocales[chr.to_sym] += 1
+           
+    end
+    vocales
+  end
+    
+    p cuentavocales!("Evelyn")
+   # p cuentavocales!("Ricardo")
+   # p cuentavocales!("Angelica")
+    p cuentavocales!("hectorouu")
