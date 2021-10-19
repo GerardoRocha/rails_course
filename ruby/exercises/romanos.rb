c66a3bd044dd1a5da8879bb95774722189fe5734
alan <alan.ortega@thincode.com>
2021-10-18 19:58:05 -0500

    Actualizacion Romanos

--- ruby/exercises/romanos.rb
+++ ruby/exercises/romanos.rb
@@ -8,54 +8,62 @@
 # 2008 is written as 2000=MM, 8=VIII; or MMVIII.
 # 1666 uses each Roman symbol in descending order: MDCLXVI.”
 
-def conversion(numdec)
-
-
-    equiv = {1 => "I",2 => "II", 3 => "III", 4 => "IV", 5 => "V", 6 => "VI", 7 => "VII", 8 => "VIII", 9 => "IX", 
+class Conversion
+    @@equiv = {1 => "I",2 => "II", 3 => "III", 4 => "IV", 5 => "V", 6 => "VI", 7 => "VII", 8 => "VIII", 9 => "IX", 
         10 => "X", 20 => "XX", 30 => "XXX", 40 => "XL", 50 => "L", 60 => "LX", 70 => "LXX", 80 => "LXXX", 90 => "XC", 
         100 => "C", 200 => "CC", 300 => "CCC", 400 => "CD", 500 => "D", 600 => "DC", 700 => "DCC", 800 => "DCCC", 900 => "CM", 
         1000 => "M", 2000 => "MM", 3000 => "MMM"}
-    romstr = /MMM|MM|M|CM|DCCC|DCC|DC|D|CD|CCC|CC|C|XC|LXXX|LXX|LX|L|XL|XXX|XX|X|IX|VIII|VII|VI|V|IV|III|II|I/
+    @@romstr = /MMM|MM|M|CM|DCCC|DCC|DC|D|CD|CCC|CC|C|XC|LXXX|LXX|LX|L|XL|XXX|XX|X|IX|VIII|VII|VI|V|IV|III|II|I/
 
-    if numdec.is_a? Integer
-        if numdec > 3999
+    def dectorom(numdecimal)
+        if numdecimal > 3999
             p "El numero no puede ser procesado"
         else
-            u = (numdec % 10)
-            d = (numdec % 100 - u)
-            c = (numdec % 1000 - (d + u))
-            m = (numdec % 10000 - (c + d + u))
+            u = (numdecimal % 10)
+            d = (numdecimal % 100 - u)
+            c = (numdecimal % 1000 - (d + u))
+            m = (numdecimal % 10000 - (c + d + u))
 
-            p "El numero romano obtenido del numero decimal #{numdec} es #{equiv[m]}#{equiv[c]}#{equiv[d]}#{equiv[u]}"
-              
+            p "El numero romano obtenido del numero decimal #{numdecimal} es #{@@equiv[m]}#{@@equiv[c]}#{@@equiv[d]}#{@@equiv[u]}"
+
         end            
-    else
-        #numdec
+    end
 
-        ary = numdec.scan(romstr) 
+
+    def romtodec(romanobase)
+        ary = romanobase.scan(@@romstr) 
+        arykv = @@equiv.to_a
         decimal = 0
 
-        equiv.to_a.each { |dec , rom| ary.each { |romb| if romb == rom
+        @@equiv.to_a.each { |dec , rom| ary.each { |romb| if romb == rom
             decimal += dec
         end} }
 
         if decimal > 3999
             p "El numero no puede ser procesado"            
         else
-            p "El numero decimal obtenido del romano #{numdec} es #{decimal}"            
+            p "El numero decimal obtenido del romano #{romanobase} es #{decimal}"            
         end
-        
+    end
+
+    def definir(numdec)
+        valorini = numdec
+        if valorini.is_a? Integer
+            dectorom(valorini)
+        else
+            romtodec(valorini)
+        end
     end
 
 end
 
-
-conversion("III")
-conversion(100)
-conversion("IX")
-conversion(1218)
-conversion("MIII")
-conversion("MCD")
-conversion(324)
-conversion("MMMCDXCIX")
-conversion(3456)
+conversion = Conversion.new
+conversion.definir("III")
+conversion.definir(100)
+conversion.definir("IX")
+conversion.definir(1218)
+conversion.definir("MIII")
+conversion.definir("MCD")
+conversion.definir(324)
+conversion.definir("MMMCDXCIX")
+conversion.definir(3456)
