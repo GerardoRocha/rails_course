3cc81cc47ca1a7e5d33c9073496057131f240aa5
MontseGlezV <montserrat.gonzalez@thincode.com>
2021-10-18 19:51:46 -0500

    mgv-exercises2

--- ruby/exercises/Romans.rb
+++ ruby/exercises/Romans.rb
@@ -1,20 +1,18 @@
-#Romans
+class RomanNum
 
-def rom_a_numerico( numerito)
+def self.rom_a_numer( numerito)
     simb = {1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
-#    multiplicador = self
     simbolo = []
     contador = 0
     simb.each do  |numd,rom|
-#        simbolo.push(rom * (multiplicador/numd)) 
-#        multiplicador = multiplicador % numd
          simbolo.push(rom * (numerito/numd)) 
          numerito = numerito % numd
         contador +=1
     end
     return simbolo.join
 end
+end
   
-p rom_a_numerico(333) 
-p rom_a_numerico(3456)
-p rom_a_numerico(7456)
+p RomanNum.rom_a_numer(333) 
+p RomanNum.rom_a_numer(3456)
+p RomanNum.rom_a_numer(7456)
