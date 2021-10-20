72cd520d18c6eb396194ffd29f32943f6d64bf9c
albertobusog <alberto.buso@thincode.com>
2021-10-20 16:48:08 -0500

    Clase romanos

--- ruby/exercises/contador_de_vocales.rb
+++ ruby/exercises/contador_de_vocales.rb
@@ -3,3 +3,16 @@
   # 'Ruby On Rails Course' => { a: 1, e: 1, i: 1, o: 2, u: 2 }
   # 'Gerardo' => { a: 1, e: 1, i: 0, o: 1, u: 0 }
   # 'BCDF' => { a: 0, e: 0, i: 0, o: 0, u: 0 }
+
+def contador!(palabra)
+  palabra.downcase!
+  vocales = { a: 0, e: 0, i: 0, o: 0, u: 0 }
+  palabra.chars.each do |chr|
+    next unless vocales.keys.include?(chr.to_sym)
+    vocales[chr.to_sym] = vocales[chr.to_sym] + 1
+  end
+  vocales
+end
+ p contador!("hola mundo")
+ p contador!("HOLA MUNDO")
+
--- ruby/exercises/romanos.rb
+++ ruby/exercises/romanos.rb
@@ -1,18 +1,50 @@
-def romanos(numeroman)
-    numero = 0 
-    numeroman.downcase! 
-    int = numeroman.length
-    valores = { i: 1, v: 5, x: 10, l: 50, c: 100, d: 500, m: 1000 }
-  (0..int).each do |i|
-    if i + 1 < int && valores[i.to_sym] < valores[i.to_sym]
-        numero = numero - valores[i.to_s]
-    else
-        numero = numero + valores[i.to_s]   
-    end  
-  end
-   numero 
+class Romanos
+    VALORES = { 
+        1000 => "m",
+        900 => "cm",
+        500 => "d",
+        400 => "cd",
+        100 => "c",
+        90 => "xc",
+        50 => "l", 
+        40 => "xl", 
+        10 => "x",
+        9 => "ix",
+        5 => "v",
+        4 => "iv", 
+        1 => "i" 
+    }
+
+  def self.romano(numeroman)  
+         roman = ''
+         while numeroman > 0
+         VALORES.each do |key, value|
+            if numeroman >= key.to_i
+                roman += VALORES[key]
+                numeroman = numeroman - key.to_i
+                break
+            end
+          end
+         end
+       roman.upcase!
+       return roman
+    end
+
+   def self.decimal(roman)
+      roman.downcase!
+      reg = /m|cm|d|cd|c|xc|l|xl|x|ix|v|iv|i/
+      temp = roman.scan(reg)
+      temp.uniq.inject(0) do |memo, value|
+       memo += temp.count(value)*VALORES.key(value).to_i
+      end
+    end
+
 end
 
-p romanos('V')
-p romanos('VIII')
-p romanos('li')
+
+p Romanos.decimal("x")
+p Romanos.decimal("M")
+p Romanos.decimal("iV")
+p Romanos.romano(5)
+p Romanos.romano(8)
+p Romanos.romano(20)
